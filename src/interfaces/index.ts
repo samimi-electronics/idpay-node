c36f9b294184d96d15e508f11dcdbd08046a40d9
hamid samimi <samimi.electronics@gmail.com>
2021-08-10 21:50:09 +0430

    exported newley created interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,3 +3,4 @@ export * from './create-payment-response';
 export * from './idpay-payment-response';
 export * from './idpay-transaction-status';
 export * from './idpay-error-codes';
+export * from './idpay-payment-verify-response';
