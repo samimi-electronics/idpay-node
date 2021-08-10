2aa495543d08ddda540952772a11bdc8e30dfcc6
hamid samimi <samimi.electronics@gmail.com>
2021-08-10 21:47:54 +0430

    created payment verfication response interface

---
+++ src/interfaces/idpay-payment-verify-response.ts
@@ -0,0 +1,22 @@
+interface IPayment {
+  track_id: string,
+  amount: number,
+  card_no: string,
+  hashed_card_no: string,
+  date: Date
+}
+
+interface IVerify {
+  date: Date
+}
+
+export interface IDPayPaymentVerifyResponse {
+  status: number,
+  track_id: number,
+  id: string,
+  order_id: string,
+  amount: number,
+  date: Date,
+  payment: IPayment,
+  verify: IVerify
+}
