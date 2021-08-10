46979f40385f6506eeb131860852718222cdd432
hamid samimi <samimi.electronics@gmail.com>
2021-08-10 21:47:31 +0430

    merged interfaces and made some optional

--- src/interfaces/idpay-payment-response.ts
+++ src/interfaces/idpay-payment-response.ts
@@ -1,18 +1,12 @@
 import { IDPayTrasactionStatus } from '.';
 
-export interface POSTidPayTrasactionResult {
+export interface IDPayTrasactionResult {
   status: IDPayTrasactionStatus;
   track_id: number;
   id: string;
   order_id: string;
-  amount: number;
-  card_no: string;
-  hashed_card_no: string;
-  date: Date;
-}
-
-export interface GETidPayTrasactionResult {
-  status: IDPayTrasactionStatus;
-  track_id: number;
-  id: string;
+  amount?: number;
+  card_no?: string;
+  hashed_card_no?: string;
+  date?: Date;
 }
